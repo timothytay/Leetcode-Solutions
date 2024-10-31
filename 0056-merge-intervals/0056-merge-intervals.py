57de980c350d84c2477564cce9777ed5d62ebf5a
timothytay <40268471+timothytay@users.noreply.github.com>
2024-10-31 20:13:05 +0000

    Time: 14 ms (24.98%), Space: 15.5 MB (86.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -8,7 +8,7 @@ class Solution(object):
         res = [intervals[0]]
         for i in range(1, len(intervals)):
             if intervals[i][0] <= res[-1][1] and intervals[i][1] > res[-1][1]:
-                res[-1] = [res[-1][0], intervals[i][1]]
+                res[-1][1] = intervals[i][1]
             elif intervals[i][0] > res[-1][1]:
                 res.append(intervals[i])
         return res
