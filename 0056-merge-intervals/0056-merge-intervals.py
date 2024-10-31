7b3e2a0359ab543f3ea875c6391632c5dac2e07d
timothytay <40268471+timothytay@users.noreply.github.com>
2024-10-31 19:57:40 +0000

    Time: 11 ms (46.37%), Space: 15.4 MB (86.68%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals = sorted(intervals, key=lambda x:x[0])
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= res[-1][1] and intervals[i][1] > res[-1][1]:
+                res[-1] = [res[-1][0], intervals[i][1]]
+            elif intervals[i][0] > res[-1][1]:
+                res.append(intervals[i])
+        return res
