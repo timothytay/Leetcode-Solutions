f2429c6de8d40b7dd7567ba835a21e63d9f5a987
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-04 20:58:47 +0000

    Time: 55 ms (26.84%), Space: 13.3 MB (70.62%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,42 +4,43 @@ class Solution(object):
         :type heights: List[List[int]]
         :rtype: List[List[int]]
         """
-        canReach = set()
-        visit = set()
-        def dfsPacific(heights, r, c, visit, prevHeight):
-            rows = len(heights)
-            cols = len(heights[0])
-            if min(r, c) < 0:
-                return True
-            if r == rows or c == cols or heights[r][c] > prevHeight or (r, c) in visit:
-                return False
-            if (r, c) in canReach:
-                return True
-            visit.add((r, c))
-            hasPath = dfsPacific(heights, r+1, c, visit, heights[r][c]) or dfsPacific(heights, r-1, c, visit, heights[r][c]) or dfsPacific(heights, r, c+1, visit, heights[r][c]) or dfsPacific(heights, r, c-1, visit, heights[r][c])
-            visit.remove((r, c))
-            return hasPath
+        if not heights:
+            return []
+        rows, cols = len(heights), len(heights[0])
+        aq = deque()
+        pq = deque()
+        aVisit = set()
+        pVisit = set()
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        for row in range(rows):
+            aq.append((row, cols-1))
+            aVisit.add((row, cols-1))
+            pq.append((row, 0))
+            pVisit.add((row, 0))
+        for col in range(cols):
+            aq.append((rows-1, col))
+            aVisit.add((rows-1, col))
+            pq.append((0, col))
+            pVisit.add((0, col))
+        while aq:
+            r, c = aq.popleft()
+            prevHeight = heights[r][c]
+            for dr, dc in directions:
+                if min(r + dr, c + dc) < 0 or r + dr == rows or c + dc == cols or heights[r+dr][c+dc] < prevHeight or (r+dr, c+dc) in aVisit:
+                    continue
+                aq.append((r+dr, c+dc))
+                aVisit.add((r+dr, c+dc))
+        while pq:
+            r, c = pq.popleft()
+            prevHeight = heights[r][c]
+            for dr, dc in directions:
+                if min(r + dr, c + dc) < 0 or r + dr == rows or c + dc == cols or heights[r+dr][c+dc] < prevHeight or (r+dr, c+dc) in pVisit:
+                    continue
+                pq.append((r+dr, c+dc))
+                pVisit.add((r+dr, c+dc))
 
-        def dfsAtlantic(heights, r, c, visit, prevHeight):
-            rows = len(heights)
-            cols = len(heights[0])
-            if r == rows or c == cols:
-                return True
-            if min(r, c) < 0 or heights[r][c] > prevHeight or (r, c) in visit:
-                return False
-            if (r, c) in canReach:
-                return True
-            visit.add((r, c))
-            hasPath = dfsAtlantic(heights, r+1, c, visit, heights[r][c]) or dfsAtlantic(heights, r-1, c, visit, heights[r][c]) or dfsAtlantic(heights, r, c+1, visit, heights[r][c]) or dfsAtlantic(heights, r, c-1, visit, heights[r][c])
-            visit.remove((r, c))
-            return hasPath
-
-        for row in range(len(heights)):
-            for col in range(len(heights[0])):
-                if dfsAtlantic(heights, row, col, visit, float('inf')) and dfsPacific(heights, row, col, visit, float('inf')):
-                    canReach.add((row, col))
-        
         res = []
-        for r, c in canReach:
-            res.append([r, c])
+        for pos in pVisit:
+            if pos in aVisit:
+                res.append(list(pos))
         return res
