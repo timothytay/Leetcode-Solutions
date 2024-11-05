703cbba7135b721362159f607d7a92565901341d
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-05 09:41:23 +0000

    Time: 30 ms (70.63%), Space: 11.8 MB (64.64%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,33 +5,22 @@ class Node(object):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-from collections import deque
+
 class Solution(object):
     def cloneGraph(self, node):
         """
         :type node: Node
         :rtype: Node
         """
-        if not node:
-            return
-        startNode = Node(node.val)
-        nodes = {}
-        nodes[node.val] = startNode
-        def bfs(node):
-            visit = set()
-            visit.add(node)
-            queue = deque()
-            queue.append(node)
-            while queue:
-                for i in range(len(queue)):
-                    curr = queue.popleft()
-                    currCopy = nodes[curr.val]
-                    for neighbor in curr.neighbors:
-                        if neighbor not in visit:
-                            visit.add(neighbor)
-                            queue.append(neighbor)
-                            nodes[neighbor.val] = Node(neighbor.val)
-                        currCopy.neighbors.append(nodes[neighbor.val])
-        bfs(node)
-        return startNode
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
         
+        return dfs(node) if node else None
