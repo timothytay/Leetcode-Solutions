6675d145da131ec811772f1cad0170cefdbe69bb
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-06 10:39:41 +0000

    Time: 0 ms (100%), Space: 11.5 MB (65.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,23 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        memo = {}
-        """
-        def robHouse(house):
-            if house >= len(nums):
-                return 0
-            if house in memo:
-                return memo[house]
-            memo[house] = nums[house] + max(robHouse(house+2), robHouse(house+3))
-            return memo[house]
-
-        return max(robHouse(0), robHouse(1))
-        """
+        
 
         if len(nums) < 2:
             return nums[0]
-        dp = nums[:]
-        for i in range(len(dp)-3, -1, -1):
-            dp[i] = dp[i] + max(dp[i+2], (dp[i+3] if i + 3 < len(dp) else 0))
+        
+        for i in range(len(nums)-3, -1, -1):
+            nums[i] = nums[i] + max(nums[i+2], (nums[i+3] if i + 3 < len(nums) else 0))
 
-        return max(dp[0], dp[1])
+        return max(nums[0], nums[1])
+
+        
+
+        
