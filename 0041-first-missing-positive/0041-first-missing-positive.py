1801a7ccfb809c387ed39fb7f460111035c6b73d
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-07 11:38:43 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = set(nums)
+
+        for i in range(1, len(nums)+1):
+            if i not in nums:
+                return i
+         
