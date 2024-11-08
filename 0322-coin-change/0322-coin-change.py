5be5b368f941b1580db88a259818e959e3b6d104
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-08 20:48:16 +0000

    Time: 934 ms (28.94%), Space: 37 MB (14.55%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,20 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        dp = [-1] * (amount + 1)
-        dp[0] = 0
-        for coin in coins:
-            if coin <= amount:
-                dp[coin] = 1
-        for i in range(1, len(dp)):
-            if dp[i] > 0:
-                continue
+from functools import lru_cache
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        @lru_cache(None)
+        def count(amountLeft):
+            if amountLeft < 0:
+                return -1
+            if amountLeft == 0:
+                return 0
+            minCoinCount = float('inf')
             for coin in coins:
-                if i - coin >= 0 and dp[i-coin] == -1:
-                    continue
-                elif i - coin >= 0 and (1 + dp[i - coin] < dp[i] or dp[i] == -1) and dp[i - coin] >= 0:
-                    dp[i] = 1 + dp[i - coin]
+                coinCount = count(amountLeft - coin)
+                if coinCount >= 0:
+                    minCoinCount = min(minCoinCount, coinCount)
+            if minCoinCount == float('inf'):
+                return -1
+            return 1 + minCoinCount
+
             
-        return dp[-1] 
+        return count(amount)
