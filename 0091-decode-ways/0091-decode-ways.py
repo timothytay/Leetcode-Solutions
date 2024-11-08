471cf96f4ccd148aabbbd495f22eb02974417e73
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-08 13:08:11 +0000

    Time: 0 ms (100%), Space: 11.8 MB (38.92%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        """
         memo = {}
 
         def decode(start):
@@ -20,3 +21,14 @@ class Solution(object):
             return memo[start]
 
         return decode(0)
+        """
+        dp = [0] * (len(s) + 1)
+        dp[-1] = 1
+        for i in range(len(dp) - 2, -1, -1):
+            if s[i] == '0':
+                continue
+            dp[i] += dp[i+1] 
+            if i + 1 < len(s) and int(s[i:i+2]) in range(10, 27):
+                dp[i] += dp[i+2]
+
+        return dp[0]
