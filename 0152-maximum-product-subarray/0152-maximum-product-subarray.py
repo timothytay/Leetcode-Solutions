958c6df2d9126eae8d38f45f6e749548d8c3ca5d
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-09 16:55:10 +0000

    Time: 16 ms (8.06%), Space: 18.6 MB (5.36%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -9,11 +9,9 @@ class Solution:
         maxDP[0], minDP[0] = nums[0], nums[0]
 
         for i in range(1, len(nums)):
-            if nums[i] == 0:
-                maxDP[i], minDP[i] = 0, 0
-            else:
-                maxDP[i] = max(nums[i] * maxDP[i-1], nums[i] * minDP[i-1], nums[i])
-                minDP[i] = min(nums[i] * maxDP[i-1], nums[i] * minDP[i-1], nums[i])
+            
+            maxDP[i] = max(nums[i] * maxDP[i-1], nums[i] * minDP[i-1], nums[i])
+            minDP[i] = min(nums[i] * maxDP[i-1], nums[i] * minDP[i-1], nums[i])
             maxProd = max(maxDP[i], maxProd)
 
         return maxProd
