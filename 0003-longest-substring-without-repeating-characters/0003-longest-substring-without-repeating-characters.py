15e7e1d2284b374ab305a6fdc35b4ce18b07c3e5
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-26 14:51:30 +0000

    Time: 19 ms (54.17%), Space: 17.3 MB (10.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,11 +4,13 @@ class Solution:
         maxLen = 0
         l = 0
         for r in range(len(s)):
-            while s[r] in window:
-                window.remove(s[l])
-                l += 1
-            
-            window.add(s[r])
+            if s[r] not in window:
+                window.add(s[r])
+            else:
+                while s[r] in window:
+                    
+                    window.remove(s[l])
+                    l += 1
+                window.add(s[r])
             maxLen = max(maxLen, len(window))
-
         return maxLen
