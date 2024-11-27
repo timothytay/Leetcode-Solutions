c590b5e01f1efc9e19134a1ef23b591671d82c49
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-27 16:17:51 +0000

    Time: 3 ms (34.58%), Space: 17.6 MB (6.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,27 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        cur = ""
-        s = []
         res = []
-        def bt(opening, closing, cur, stack):
+        def bt(opening, closing, curOpening, cur):
+            
+            if opening > 0:
+                cur += '('
+                bt(opening - 1, closing, curOpening + 1, cur)
+                cur = cur[:-1]
+            if curOpening > 0:
+                cur += ')'
+                bt(opening, closing - 1, curOpening - 1, cur)
+                cur = cur[:-1]
             if opening == 0 and closing == 0:
                 res.append(cur)
-             
-                return
-            if opening > 0:
-                tempStack = stack
-                stack.append('(')
-                cur += '('
-                bt(opening - 1, closing, cur, stack)
-                cur = cur[:-1]
-                stack = tempStack
-            if stack and stack[-1] == '(':
-                tempStack = stack
-                cur += ')'
-                stack.pop()
-                bt(opening, closing - 1, cur, stack[:])
-                cur = cur[:-1]
-                stack = tempStack
-            return
-        bt(n, n, cur, s)
+        bt(n, n, 0, "")
         return res
