dd3ce2431654b2c5982aeaf019a668e0f78b2646
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-30 17:47:58 +0000

    Time: 3 ms (94.09%), Space: 20.4 MB (5.24%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -9,27 +9,16 @@ class Solution:
         # for each node, check largest length with traversal
         # get maximum depth of left subtree and max of right subtree
         # start at 0 when reaching leaf node, add 1 per future node to get edges
-        cache = {}
-        def getMaxDepth(node):
-            if not node or (not node.left and not node.right):
-                return 0
-            else:
-                if node in cache:
-                    return cache[node]
-                else:
-                    cache[node] = 1 + max(getMaxDepth(node.left), getMaxDepth(node.right))
-                    return cache[node]
-        def dfs(node):
-            if node and node.left and node.right:
-                maxDiameter = getMaxDepth(node.left) + getMaxDepth(node.right) + 2
-            elif node and node.left:
-                maxDiameter = getMaxDepth(node.left) + 1
-            elif node and node.right:
-                maxDiameter = getMaxDepth(node.right) + 1
-            else:
-                return 0
-
-            return max(maxDiameter, dfs(node.left), dfs(node.right))
-            
-        return dfs(root)
+        diameter = 0
         
+        def longestPath(root):
+            if not root:
+                return 0
+            nonlocal diameter
+            leftLongest = longestPath(root.left)
+            rightLongest = longestPath(root.right)
+            diameter = max(diameter, leftLongest + rightLongest)
+            return 1 + max(leftLongest, rightLongest)
+
+        longestPath(root)
+        return diameter
