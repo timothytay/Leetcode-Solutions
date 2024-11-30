c7ece264a9295fcf96763b78df1d84f412c9f35f
timothytay <40268471+timothytay@users.noreply.github.com>
2024-11-30 11:31:37 +0000

    Time: 3 ms (71.27%), Space: 17.3 MB (5.25%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,27 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode()
-        cur = dummy
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+         # 2 pointers, one at current of each node
+         # add the values 
+         # if the values add to more than 10, set the current value to sum - 10
+         # carry variable
+         # edge cases: trailing nodes for one list after other pointer goes out of bounds
+         # another: carry after both nodes finish (add another node)
+        
+        dummy = ListNode(0)
         cur1 = l1
         cur2 = l2
+        res = dummy
         carry = 0
-        while cur1 or cur2 or carry != 0:
-            cur1Val = cur1.val if cur1 else 0
-            cur2Val = cur2.val if cur2 else 0
-            res = cur1Val + cur2Val + carry
-            carry = res // 10
-            cur.next = ListNode(res % 10)
-            cur = cur.next
-            cur1 = cur1.next if cur1 else None
-            cur2 = cur2.next if cur2 else None
+        while cur1 and cur2:
+            added = cur1.val + cur2.val + carry
+            carry = 0
+            if added >= 10:
+                added -= 10
+                carry = 1
+            res.next = ListNode(added)
+            cur1, cur2, res = cur1.next, cur2.next, res.next
+
+        leftover = cur1 if cur1 else cur2
+        while leftover or carry > 0:
+            added = (leftover.val if leftover else 0) + carry
+            carry = 0
+            if added >= 10:
+                added -= 10
+                carry = 1
+            res.next = ListNode(added)
+            res = res.next
+            leftover = leftover.next if leftover else None
+
         return dummy.next
