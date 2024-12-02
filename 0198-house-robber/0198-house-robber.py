38711d6619e90ac5ae6c601b02dcb29ddc7c0032
timothytay <40268471+timothytay@users.noreply.github.com>
2024-12-02 18:02:40 +0000

    Time: 2 ms (9.23%), Space: 17.3 MB (5.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,12 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        rob1, rob2 = 0, 0
-        for i in range(len(nums)):
-            temp = rob2
-            rob2 = max(rob1 + nums[i], rob2)
-            rob1 = temp
-        return rob2
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        cache = {}
+        def robHouse(i):
+            nonlocal nums
+            if i >= len(nums):
+                return 0
+            if i in cache:
+                return cache[i]
+            cache[i] = nums[i] + max(robHouse(i+2), robHouse(i+3))
+            return cache[i]
+        return max(robHouse(0), robHouse(1))
