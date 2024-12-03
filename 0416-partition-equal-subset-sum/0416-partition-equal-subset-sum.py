3e606715b6fb8f08aa899b6132ee1b8a2828643c
timothytay <40268471+timothytay@users.noreply.github.com>
2024-12-03 12:07:46 +0000

    Time: 542 ms (59.54%), Space: 157.8 MB (10.68%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,17 +1,22 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         numSum = sum(nums)
+        target = numSum // 2
+        if numSum % 2:
+            return False
         #cache = {}
         @lru_cache(maxsize = None)
-        def helper(sum1, i):
+        def helper(target, i):
             nonlocal nums, numSum
-            if sum1 == numSum - sum1:
+            if target == 0:
                 return True
-            if i >= len(nums):
+            if i >= len(nums) or target < 0:
                 return False
 
             #if (sum1, i) in cache:
             #    return cache[(sum1, i)]
             #cache[(sum1, i)] = helper(sum1 - nums[i], i + 1) or helper(sum1, i + 1)
-            return helper(sum1 - nums[i], i + 1) or helper(sum1, i + 1)
-        return helper(sum(nums), 0)
+            return helper(target - nums[i], i + 1) or helper(target, i + 1)
+        
+
+        return helper(target, 0)
