e37686cb09051f33877de0feee2c31c47565e87e
timothytay <40268471+timothytay@users.noreply.github.com>
2024-12-03 13:28:05 +0000

    Time: 7 ms (74.45%), Space: 21.4 MB (5.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,14 +1,18 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        intervals = sorted(intervals, key=lambda x:x[0])
-        res = [intervals[0]]
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= res[-1][1] and intervals[i][1] > res[-1][1]:
-                res[-1][1] = intervals[i][1]
-            elif intervals[i][0] > res[-1][1]:
-                res.append(intervals[i])
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sort the intervals based on start 
+        # for each interval which does not overlap with previous
+        # keep a running largest end index to see if it continues to overlap
+        # add then to a new result array
+        # sorting them takes into account the possibility of next start being less than prev end but also less than prev start making them potentially not overlap 
+        sortedInts = sorted(intervals, key=lambda x:x[0])
+        res = []
+        for start, end in sortedInts:
+            if not res or res[-1][1] < start:
+                res.append([start, end])
+            elif end <= res[-1][1]:
+                continue
+            else:
+                res[-1][1] = end
         return res
+
