1c7c1018a119b7f459cadce7b478754ae96db6bd
timothytay <40268471+timothytay@users.noreply.github.com>
2024-12-04 14:51:04 +0000

    Time: 30 ms (36.17%), Space: 24.9 MB (50.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        suffix = [0] * len(nums)
-        suffix[-1] = 1
-        for i in range(len(suffix) - 2, -1, -1):
-            suffix[i] = suffix[i+1] * nums[i+1]
-        tmp = nums[0]
-        nums[0] = 1
+        
+        # algorithm outline
+        # compute prefix products
+        # compute suffix products
+        # multiply prefix and suffix products for each index
+        # return 
+
+        prefix = [1] * len(nums)
         for i in range(1, len(nums)):
-            res = nums[i-1] * tmp 
-            tmp = nums[i]
-            nums[i] = res
-        for i in range(len(suffix)):
-            suffix[i] = suffix[i] * nums[i]
+            prefix[i] = prefix[i-1] * nums[i-1]
+        # 1, 1, 2, 6
+
+        suffix = [1] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            suffix[i] = suffix[i+1] * nums[i+1]
+        # 24, 12, 4, 1
+
+        for i in range(len(nums)):
+            suffix[i] *= prefix[i]
+        
         return suffix
