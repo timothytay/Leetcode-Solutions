d02a98e7e99e47c9068a07ed5d228d4438b5d885
timothytay <40268471+timothytay@users.noreply.github.com>
2024-12-05 11:28:21 +0000

    Time: 283 ms (20.1%), Space: 33.6 MB (6.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,22 +4,24 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        islands = 0
-        def dfs(grid, r, c):
-            rows = len(grid)
-            columns = len(grid[0])
-            if min(r, c) < 0 or r >= rows or c >= columns or grid[r][c] == '0':
-                return
-            grid[r][c] = '0'
-            dfs(grid, r+1, c)
-            dfs(grid, r-1, c)
-            dfs(grid, r, c+1)
-            dfs(grid, r, c-1)
+        seen = set()
+        def dfs(grid, row, col):
+            if (row, col) in seen:
+                return 
+            ROWS, COLS = len(grid), len(grid[0])
+            if min(row, col) < 0 or row >= ROWS or col >= COLS or grid[row][col] == '0':
+                return 
+            seen.add((row, col))
+            dfs(grid, row + 1, col)
+            dfs(grid, row - 1, col)
+            dfs(grid, row, col + 1)
+            dfs(grid, row, col - 1)
             return 
-
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == '1':
-                    islands += 1
-                    dfs(grid, row, col)
-        return islands
+        count = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) not in seen and grid[r][c] == '1':
+                    count += 1
+                    dfs(grid, r, c)
+        return count
