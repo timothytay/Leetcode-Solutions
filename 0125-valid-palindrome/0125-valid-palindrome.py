8a70c555b5bc23d1bffb26310b274d49692e37c1
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-22 15:45:42 +0300

    Time: 18 ms (45.45%), Space: 12.7 MB (90.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,31 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # algorithm outline
-        # 2 pointers, left and right
-        # per iteration, compare left and right and if not same return false
-        # if not alphanumeric, skip
-
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         l, r = 0, len(s) - 1
         while l <= r:
             if not s[l].isalnum():
                 l += 1
+                continue
             elif not s[r].isalnum():
                 r -= 1
-            else:
-                if s[l].lower() != s[r].lower():
+                continue
+            elif s[l].isalpha() and s[r].isalpha():
+                if s[l].lower() == s[r].lower():
+                    l += 1
+                    r -= 1
+                    continue
+                else:
                     return False
-                l += 1
-                r -= 1
+            elif s[l].isnumeric() and s[r].isnumeric():
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                    continue
+                else:
+                    return False
+            else:
+                return False
         return True
