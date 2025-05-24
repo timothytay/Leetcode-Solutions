5fd2f47fd899aa440000a55bac5abfcc4ca4693d
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-24 15:47:16 +0300

    Time: 11 ms (97.36%), Space: 18.1 MB (18.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,18 @@
+from collections import defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        count1, count2 = [0] * 26, [0] * 26
+        s1Freqs = [0] * 26
+        s2Freqs = [0] * 26
         for i in range(len(s1)):
-            count1[ord(s1[i]) - ord('a')] += 1
-            count2[ord(s2[i]) - ord('a')] += 1
-        l = 0
-        match = 0
-        for i in range(26):
-            if count1[i] == count2[i]:
-                match += 1
-        for r in range(len(s1), len(s2)):
-            if match == 26:
+            s1Freqs[ord(s1[i]) - ord('a')] += 1
+            s2Freqs[ord(s2[i]) - ord('a')] += 1
+        if s1Freqs == s2Freqs:
+            return True
+        for l in range(0, len(s2) - len(s1)):
+            s2Freqs[ord(s2[l]) - ord('a')] -= 1
+            s2Freqs[ord(s2[l + len(s1)]) - ord('a')] += 1
+            if s1Freqs == s2Freqs:
                 return True
-            count2[ord(s2[l]) - ord('a')] -= 1
-            if count2[ord(s2[l]) - ord('a')] == count1[ord(s2[l]) - ord('a')]:
-                match += 1
-            if count2[ord(s2[l]) - ord('a')] + 1 == count1[ord(s2[l]) - ord('a')]:
-                match -= 1  
-            l += 1
-            count2[ord(s2[r]) - ord('a')] += 1
-            if count2[ord(s2[r]) - ord('a')] == count1[ord(s2[r]) - ord('a')]:
-                match += 1
-            if count2[ord(s2[r]) - ord('a')] - 1 == count1[ord(s2[r]) - ord('a')]:
-                match -= 1
-            
-        return match == 26
+        return False
