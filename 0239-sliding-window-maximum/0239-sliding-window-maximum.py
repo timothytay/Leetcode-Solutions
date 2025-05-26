3b5abae36240de59f5ea83fc5dbc0b98531d069a
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-26 21:35:33 +0300

    Time: 223 ms (24.66%), Space: 31.8 MB (49.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -11,9 +11,12 @@ class Solution:
                 while dq and nums[i] >= dq[-1][0]:
                     dq.pop() 
                 dq.append((nums[i], i))
-            if i >= k - 1:
-                
+            if i >= k - 1:    
                 ans.append(dq[0][0])
         return ans
                 
+    # remove ones that fall out of window on left
+    # add to right when current is smaller because it might be used later
+    # if current is bigger than rightmost, remove right until it is smaller then add
+    # this is because if it is bigger and more recent, the previous rightmost will never be used
 
