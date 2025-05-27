8c5c2de74dd36ff381cd091d83818bcd9eff09d8
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-27 14:23:54 +0300

    Time: 35 ms (24.02%), Space: 37.5 MB (23.87%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,19 +1,11 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        prefixMin = nums[:]
-        for i in range(len(nums)):
-            if i >= 1 and nums[i] < prefixMin[i-1]:
-                prefixMin[i] = nums[i]
-            else:
-                prefixMin[i] = prefixMin[i-1 if i > 0 else i]
-        postfixMax = nums[:]
-        for i in range(len(nums) - 1, -1, -1):
-            if i < len(nums) - 1 and nums[i] > postfixMax[i+1]:
-                postfixMax[i] = nums[i]
-            else:
-                postfixMax[i] = postfixMax[i+1 if i+1 <= len(nums) - 1 else i]
-        for i in range(len(nums)):
-            if i > 0 and i < len(nums) - 1:
-                if prefixMin[i-1] < nums[i] < postfixMax[i+1]:
-                    return True
+        first = second = float('inf')
+        for num in nums:
+            if num < first:
+                first = num
+            elif first < num < second:
+                second = num
+            elif num > second:
+                return True
         return False
