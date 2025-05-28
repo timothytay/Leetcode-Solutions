a93e2af170f64822afc021fffed7833bac0c03db
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-28 17:23:07 +0300

    Time: 223 ms (17.13%), Space: 33 MB (39.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,24 +1,30 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        
-        stack = []  # Stack to store indices of the bars
-        curMax = 0
-        n = len(heights)
-        
-        for i in range(n):
-            # While the current bar is less than the bar at the top of the stack
-            while stack and heights[i] < heights[stack[-1]]:
-                top = stack.pop()
-                # Calculate the width
-                width = i if not stack else i - stack[-1] - 1
-                # Calculate the area with heights[top] as the smallest bar
-                curMax = max(curMax, heights[top] * width)
-            stack.append(i)  # Push current index to stack
-        
-        # Process remaining bars in the stack
-        while stack:
-            top = stack.pop()
-            width = n if not stack else n - stack[-1] - 1
-            curMax = max(curMax, heights[top] * width)
-        
-        return curMax
+        stack = []
+        right, left = [len(heights) - 1] * len(heights), [0] * len(heights)
+
+        for i in range(len(heights)):
+            if not stack or heights[i] >= heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and heights[stack[-1]] > heights[i]:
+                    right[stack[-1]] = i - 1
+                    stack.pop()
+                stack.append(i)
+
+        stack = []
+        for i in range(len(heights) - 1, -1, -1):
+            if not stack or heights[i] >= heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and heights[stack[-1]] > heights[i]:
+                    left[stack[-1]] = i + 1
+                    stack.pop()
+                stack.append(i)
+
+        largest = 0
+
+        for i in range(len(heights)):
+            largest = max(largest, heights[i] * (right[i] - left[i] + 1))
+
+        return largest
