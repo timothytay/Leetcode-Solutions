633681b8e65a5af4d85e47f522439a4b38dd5a95
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-29 17:24:07 +0300

    Time: 0 ms (100%), Space: 18.2 MB (40.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,28 +1,25 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-        merged = []
-        i, j = 0, 0
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                merged.append(nums1[i])
-                i += 1
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        if len(A) > len(B):
+            A, B = B, A
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        lo, hi = 0, len(A) - 1
+        while True:
+            i = lo + (hi - lo) // 2
+            j = half - i - 2 
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i+1] if i + 1 < len(A) else float('inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j+1] if j + 1 < len(B) else float('inf')
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                else:
+                    return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                hi = i - 1
             else:
-                merged.append(nums2[j])
-                j += 1
-        while i < len(nums1):
-            merged.append(nums1[i])
-            i += 1
-        while j < len(nums2):
-            merged.append(nums2[j])
-            j += 1
-        if len(merged) % 2 == 0:
-            low = float(merged[len(merged)/2-1])
-            high = float(merged[len(merged)/2])
-            return (high + low) / 2.0
-        else:
-            return merged[len(merged)//2]
+                lo = i + 1
