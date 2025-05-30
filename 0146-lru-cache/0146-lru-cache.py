6167ebcf9ea6486fd81caef1de1bffadf86430cf
timothytay <40268471+timothytay@users.noreply.github.com>
2025-05-30 14:08:55 +0300

    Time: 157 ms (36.59%), Space: 78.7 MB (27.36%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,80 +1,56 @@
-class ListNode():
-    def __init__(self, value):
+class ListNode:
+    def __init__(self, val):
         self.next = None
         self.prev = None
-        self.val = value
-        self.key = None
+        self.val = val
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.size = 0
-        self.head = None
-        self.tail = self.head
         self.cache = {}
+        self.head, self.tail = ListNode(0), ListNode(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.capacity = capacity
+        self.len = 0
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            cur = self.cache[key]
-            self.removeNode(cur)
-            self.addToEnd(cur)
-            return cur.val
+            val = self.cache[key].val
+            self.remove(self.cache[key])
+            self.insertFront(self.cache[key], self.head)
+            return self.cache[key].val[0]
         else:
             return -1
 
     def put(self, key: int, value: int) -> None:
-        
         if key in self.cache:
-            cur = self.cache[key]
-            self.removeNode(cur)
-            self.addToEnd(cur)
-            cur.val = value
+            self.cache[key].val = (value, key)
+            self.remove(self.cache[key])
+            self.insertFront(self.cache[key], self.head)
         else:
-            if self.size >= self.capacity and key not in self.cache:
-                self.cache.pop(self.head.key)
-                self.removeFromFront()
-            
-            self.addToEnd(ListNode(value))
-            self.tail.key = key
-            self.cache[key] = self.tail
+            new = ListNode((value, key))
+            self.cache[key] = new
+            self.insertFront(new, self.head)
+            self.len += 1
+            if self.len > self.capacity:
+                old = self.tail.prev
+                self.remove(old)
+                self.cache.pop(old.val[1], None)
+                self.len -= 1
 
-    def removeNode(self, node):
-        prev = node.prev
-        nxt = node.next
-        if prev and nxt:
-            prev.next = nxt
-            nxt.prev = prev
-        elif not prev and not nxt:
-            self.head = self.tail = None
-        elif not prev:
-            self.head = self.head.next
-            self.head.prev = None
-        elif not nxt:
-            self.tail = self.tail.prev
-            self.tail.next = None
-        self.size -= 1
 
-    def addToEnd(self, node):
-        if not self.head:
-            self.head = self.tail = node
-        else:
-            self.tail.next = node
-            tmp = self.tail
-            self.tail = self.tail.next
-            self.tail.prev = tmp
-        self.size += 1
+    def insertFront(self, node: ListNode, head: ListNode) -> None:
+        tmp = self.head.next 
+        self.head.next = node
+        node.prev = self.head
+        tmp.prev = node
+        node.next = tmp
 
-    def removeFromFront(self):
-        if not self.head:
-            return
-        elif self.size == 1:
-            self.head = self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-        self.size -= 1
-        
+    def remove(self, node: ListNode) -> None:
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
