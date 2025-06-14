b2ec2374050df89736790d124e6f7f58b70a481a
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-14 15:55:31 +0300

    Time: 1502 ms (5.04%), Space: 19.4 MB (30.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,40 +1,26 @@
-from collections import defaultdict
-
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        adjList = {}
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = defaultdict(list)
+        node_list = set()
         for course, prereq in prerequisites:
-            if course not in adjList:
-                adjList[course] = []
-            if prereq not in adjList:
-                adjList[prereq] = []
-            adjList[course].append(prereq)
-
-        visited = {}
-        def dfs(node, visit):
-            if node in visit:
-                if visit[node] == 1:
-                    return False
-                if visit[node] == 2:
-                    return True
-            visit[node] = 1
-            for neighbor in adjList[node]:
-                if not dfs(neighbor, visit):
-                    return False
-            visit[node] = 2
-            return True
-
-        for course in range(numCourses):
-            if course not in adjList:
-                continue
-            if not dfs(course, visited):
+            adj_list[prereq].append(course)
+            node_list.add(course)
+            node_list.add(prereq)
+        
+        def cycle(node, adj_list, seen, good):
+            if node in good:
                 return False
-            
+            if node in seen:
+                return True
+            seen.add(node)
+            for nei in adj_list[node]:
+                if cycle(nei, adj_list, seen, good):
+                    return True
+            seen.remove(node)
+            good.add(node)
+            return False
+
+        for course in node_list:
+            if cycle(course, adj_list, set(), set()):
+                return False
         return True
-        
-        
