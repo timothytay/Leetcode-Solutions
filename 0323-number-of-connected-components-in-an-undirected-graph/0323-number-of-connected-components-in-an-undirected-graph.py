c5732eb51046e7b67412d683500bdcee8f478203
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-14 19:31:02 +0300

    Time: 7 ms (52.82%), Space: 19.7 MB (17.05%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,26 +1,23 @@
-class Solution(object):
-    def countComponents(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: int
-        """
-        adj = {key:[] for key in range(n)}
-        for node1, node2 in edges:
-            
-            adj[node1].append(node2)
-            adj[node2].append(node1)
-        visit = set()
-        def dfs(node):
-            if node in visit:
-                return
-            visit.add(node)
-            for nei in adj[node]:
-                dfs(nei)
-            return
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj_list = {i:[] for i in range(n)}
+        for a, b in edges:
+            adj_list[a].append(b)
+            adj_list[b].append(a)
+
+        def dfs(node, seen):
+            if node in seen:
+                return 
+            seen.add(node)
+            for nei in adj_list[node]:
+                dfs(nei, seen)
+
         count = 0
-        for node in range(n):
-            if node not in visit:
-                dfs(node)
+        seen = set()
+
+        for node in adj_list.keys():
+            if node not in seen:
+                dfs(node, seen)
                 count += 1
+
         return count
