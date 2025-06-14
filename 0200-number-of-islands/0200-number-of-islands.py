916f4d06a545c84a3b0329c81d18d3fc7bd8d208
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-14 07:42:50 +0300

    Time: 238 ms (76.56%), Space: 20.1 MB (82.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,22 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        seen = set()
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
         def dfs(grid, row, col):
-            if (row, col) in seen:
-                return 
-            ROWS, COLS = len(grid), len(grid[0])
-            if min(row, col) < 0 or row >= ROWS or col >= COLS or grid[row][col] == '0':
-                return 
-            seen.add((row, col))
+            ROWS, COLS = len(grid) - 1, len(grid[0]) - 1
+            if row < 0 or row > ROWS or col < 0 or col > COLS or grid[row][col] != '1':
+                return
+            grid[row][col] = '0'
             dfs(grid, row + 1, col)
             dfs(grid, row - 1, col)
             dfs(grid, row, col + 1)
             dfs(grid, row, col - 1)
-            return 
-        count = 0
-        ROWS, COLS = len(grid), len(grid[0])
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) not in seen and grid[r][c] == '1':
-                    count += 1
-                    dfs(grid, r, c)
-        return count
+
+        number = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1':
+                    dfs(grid, row, col)
+                    number += 1
+        return number 
+        
