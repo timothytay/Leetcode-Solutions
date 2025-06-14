27fb3e9a600266ff203b200972ad4b3833d8d394
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-14 10:58:18 +0300

    Time: 42 ms (66.12%), Space: 18.3 MB (16.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,26 +1,38 @@
 """
 # Definition for a Node.
-class Node(object):
+class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution(object):
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        oldToNew = {}
-
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for neighbor in node.neighbors:
-                copy.neighbors.append(dfs(neighbor))
-            return copy
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         
-        return dfs(node) if node else None
+        if not node:
+            return None
+
+        node_map = {}
+        seen = set()
+
+        def bfs(node):
+            q = deque()
+            q.appendleft(node)
+            node_map[node] = Node(node.val)
+
+            while len(q) > 0:
+                for i in range(len(q)):
+                    cur = q.pop()
+                    if cur not in seen:
+                        seen.add(cur)
+                        for nei in cur.neighbors:
+                            if nei not in node_map:
+                                node_map[nei] = Node(nei.val)
+                            node_map[cur].neighbors.append(node_map[nei])
+                            if nei not in seen:
+                                q.appendleft(nei)
+
+        bfs(node)
+
+        return node_map[node]
