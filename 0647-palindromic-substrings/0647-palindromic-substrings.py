5fd311866fba059761d9d95926d30a0b0a6bb3ed
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-15 19:42:56 +0300

    Time: 476 ms (12.23%), Space: 25.6 MB (13.35%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,20 +1,31 @@
-class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
         count = 0
-
+        dp = [[False] * len(s) for _ in range(len(s))]
         for i in range(len(s)):
-            for j in range(2 if i + 1 < len(s) else 1):
-                l, r = i, i+j
-                if l == r or s[l] == s[r]:
-                    while s[l] == s[r]:
+            dp[i][i] = True
+            count += 1
+        for i in range(len(s) - 1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+                count += 1
+        for i in range(3, len(s) + 1):
+            if i % 2:
+                half = i // 2
+                for j in range(len(s)):
+                    if (j - half >= 0 and j + half < len(s) and s[j - half] == s[j + half] and dp[j - half + 1][j + half - 1]):
+                        dp[j - half][j + half] = True
                         count += 1
-                        if l - 1 >= 0 and r + 1 < len(s):
-                            l -= 1
-                            r += 1
-                        else:
-                            break
+            else:
+                half = i // 2 - 1
+                for j in range(len(s)):
+                    if (j - half >= 0 and j + 1 + half < len(s) and s[j - half] == s[j + 1 + half] and dp[j - half + 1][j + 1 + half - 1]):
+                        dp[j - half][j + 1 + half] = True
+                        count += 1
+
+
+
+
         return count
+        
