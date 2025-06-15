68aa636d0e456752da96641b66bbcb672278ef39
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-15 20:23:16 +0300

    Time: 0 ms (100%), Space: 17.9 MB (31.3%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,34 +1,18 @@
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        """
-        memo = {}
-
-        def decode(start):
-            if start in memo:
-                return memo[start]
-            if start == len(s):
-                return 1
-            if s[start] == '0':
-                return 0
-            total = decode(start+1)
-            if start + 1 < len(s) and int(s[start:start+2]) in range(10, 27):
-                total += decode(start+2)
-            memo[start] = total
-            return memo[start]
-
-        return decode(0)
-        """
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
         dp = [0] * (len(s) + 1)
         dp[-1] = 1
-        for i in range(len(dp) - 2, -1, -1):
+
+        for i in range(len(s) - 1, -1, -1):
             if s[i] == '0':
                 continue
-            dp[i] += dp[i+1] 
-            if i + 1 < len(s) and int(s[i:i+2]) in range(10, 27):
-                dp[i] += dp[i+2]
+            if i + 1 < len(s):
+                if s[i] == '1' or (s[i] == '2' and int(s[i+1]) <= 6):
+                    dp[i] = dp[i+1] + dp[i+2]
+                else:
+                     dp[i] = dp[i+1] 
+            else:
+                dp[i] = dp[i+1] 
 
         return dp[0]
