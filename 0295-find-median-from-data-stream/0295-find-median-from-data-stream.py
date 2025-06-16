18c342e1c21d8cfd0a1b1c348298a6936ba54f74
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-16 16:29:37 +0300

    Time: 162 ms (37.24%), Space: 38.8 MB (99.37%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,40 +1,31 @@
-import heapq
-
 class MedianFinder:
 
     def __init__(self):
-        # max heap so push negative numbers
-        self.lower = []
-        # min heap so default usage with heapq
-        self.upper = []
-        heapq.heapify(self.lower)
-        heapq.heapify(self.upper)
-        self.count = 0
+        self.upper = [] # min-heap
+        self.lower = [] # max-heap so negative numbers
+        self.size = 0
 
     def addNum(self, num: int) -> None:
-        if not self.lower and not self.upper:
-            heapq.heappush(self.lower, -num)
+        self.size += 1
+        if not self.lower or num >= -1 * self.lower[0]:
+            heapq.heappush(self.upper, num)
         else:
-            if self.lower and -num < self.lower[0]:
-                heapq.heappush(self.upper, num)
-            else:
-                heapq.heappush(self.lower, -num)
-        self.count += 1
-        while len(self.lower) - len(self.upper) > 1:
-            tmp = -heapq.heappop(self.lower)
-            heapq.heappush(self.upper, tmp)
-        while len(self.upper) - len(self.lower) > 1:
-            tmp = heapq.heappop(self.upper)
-            heapq.heappush(self.lower, -tmp)
+            heapq.heappush(self.lower, -num)
+        if len(self.upper) - len(self.lower) > 1:
+            num_to_swap = heapq.heappop(self.upper)
+            heapq.heappush(self.lower, -1 * num_to_swap)
+        elif len(self.lower) - len(self.upper) > 1:
+            num_to_swap = -1 * heapq.heappop(self.lower)
+            heapq.heappush(self.upper, num_to_swap)
 
     def findMedian(self) -> float:
-        if self.count % 2 == 0:
-            return (-self.lower[0] + self.upper[0]) / 2
-        else:
+        if self.size % 2:
             if len(self.upper) > len(self.lower):
                 return self.upper[0]
             else:
-                return -self.lower[0]
+                return -1 * self.lower[0]
+        else:
+            return (self.upper[0] + -1 * self.lower[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
