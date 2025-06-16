165afc659db580f30bb78a21d0f88c6cf0f9b2f3
timothytay <40268471+timothytay@users.noreply.github.com>
2025-06-16 10:36:02 +0300

    Time: 4 ms (18.5%), Space: 17.6 MB (98.73%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,30 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i, j = len(a) - 1, len(b) - 1
+        carry = False
+        res = ""
+        while i >= 0 or j >= 0:
+            result = '0'
+            a_digit = a[i] if i >= 0 else '0'
+            b_digit = b[j] if j >= 0 else '0'
+            if a_digit == '1' and b_digit == '1':
+                result = '0'
+                if carry:
+                    result = '1'
+                carry = True
+            elif a_digit == '1' or b_digit == '1':
+                result = '1'
+                if carry:
+                    result = '0'
+            else:
+                result = '0'
+                if carry:
+                    result = '1'
+                carry = False
+            res = result + res
+            i -= 1
+            j -= 1
+        if carry:
+            res = '1' + res
+        return res
+
